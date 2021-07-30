fcf51d21d2d3ebf9293688b2284ac189e8a841f4
Shang-QY <55442231+Shang-QY@users.noreply.github.com>
2021-07-30 19:16:08 +0800

    add readme

---
+++ lab4/env.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "util.h"
+#include "symbol.h"
+#include "env.h"
+
+/*Lab4: Your implementation of lab4*/
+
+E_enventry E_VarEntry(Ty_ty ty)
+{
+	return NULL;
+}
+
+E_enventry E_FunEntry(Ty_tyList formals, Ty_ty result)
+{
+	return NULL;
+}
+
+S_table E_base_tenv(void)
+{
+	return NULL;
+}
+
+S_table E_base_venv(void)
+{
+	return NULL;
+}
